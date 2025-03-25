ef70360633d633967749ed331364b62dd1895fcd
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-03-25 15:44:39 +0200

    321
    312

--- js/script.js
+++ js/script.js
@@ -1,15 +1,102 @@
-const students = ["Mykola", "Lev", "Vlad D.", "Nazar", "Rostislav", "Vlad"]
+// const kava = "кава";
+// const chay = "чай";
+// const sik = "сік";
+// const result = prompt(`Виберіть напій ${kava}, ${chay} або ${sik}`);
+// switch (result.toLowerCase()) {
+//     case kava:
+//         alert(`Дякую за вибір (${kava})`)
+//         console.log("Вибрано каву");
+//         break;
+//         case chay:
+//         alert(`Дякую за вибір (${chay})`)
+//         console.log("Вибрано Чай");
+//         break;
+//         case sik:
+//         alert(`Дякую за вибір (${sik})`)
+//         console.log("Вибрано Сік");
+//         break;
+//     default:
+//         alert("Такого напою не існує або немає в наявності")
+//         console.log("Помилка");
+        
+// }
 
-console.log(students);
-students.push("Yura")//Метод додає в кінець массиву//
-console.log(students);
-students.unshift("Zoryana", "123")//Метод додає на початок массиву//
-console.log(students);
-students.shift();
-students.shift();
-console.log(students);
-students.pop()//Метод видаляє останній елемент з массиву//
-console.log(students);
+
+//2Створіть змінну для зберігання введеного рядка, який може бути днем тижня. Якщо це робочий день — виведіть повідомлення про робочий день, якщо вихідний — про вихідний.
+
+// const days = "Понеділок Вівторок Середа Четвер П'ятниця";
+// const day = "Субота Неділя"
+// const inp = prompt("Виберіть день тижня, від Понеділка до П'ятниці");
+// if (days.includes(inp)) {
+//     alert("Ви обрали робочий день")
+// } else if (day.includes(inp)) {
+//     alert ("Ви обрали вихідний день")
+// } else {
+//     alert ("Такого дня не існує")
+// }
+
+
+//3Створіть змінну для зберігання номера місяця. За номером місяця визначайте пору року і виводьте відповідне повідомлення.
+
+// const winter = [12, 1, 2];
+// const spring = [3, 4, 5];
+// const summer = [6, 7, 8];
+// const fall = [9, 10, 11];
+// const input = Number(prompt("Напишіть число від (1-12), щоб дізнатися пору року"));
+// if (winter.includes(input)) {
+//     alert("Ви обрали Зиму");
+// } else if (spring.includes(input)) {
+//     alert("Ви обрали Весну");
+// } else if (summer.includes(input)) {
+//     alert("Ви обрали Літо");
+// } else if (fall.includes(input)) {
+//     alert("Ви обрали Осінь");
+// } else {
+//     alert("Помилка Введіть число від (1-12)");
+// }
+
+//4Створіть змінну для зберігання назви кольору. Виводьте повідомлення відповідно до вибраного кольору: якщо "червоний" — "стоп", "зелений" — "йти", "жовтий" — "чекати".
+
+// const red = "червоний";
+// const yell = "жовтий";
+// const gre = "зелений";
+// const result = prompt(`Виберіть колір ${red} ${yell} або ${gre}`);
+// if (result.toLowerCase().includes(gre)) {
+//     alert ("ЙТИ")
+// } else if (result.toLowerCase().includes(yell)) {
+//     alert("ЧЕКАТИ")
+// } else if (result.toLowerCase().includes(red)) {
+//     alert ("СТОП")
+// } else {
+//     alert(`Помилка, введіть ${red} ${yell} або ${gre} колір`)
+// }
+
+
+//5Створіть змінні для зберігання двох чисел та оператора (як у списку select). Виконайте відповідну операцію та виведіть результат. У випадку ділення на нуль — виведіть попередження.
+
+const namber = Number(prompt("Введіть число"));
+const oper = prompt("Оберіть оператор (+ - * /)");
+const number = Number(prompt('Введіть число'))
+switch (oper) {
+    case "+":
+        alert(namber + number);
+        break;
+    case "-":
+        alert(namber - number);
+        break;
+    case "*":
+        alert(namber * number);
+        break;
+    case "/":
+        if (number === 0) {
+            alert("На нуль ділити не можна");
+        } else {
+            alert(namber / number)
+        }
+        break;
+    default:
+        alert ("Помилка введіть вірні данні")
+}
 
 
 
