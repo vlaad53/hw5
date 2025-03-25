d091219e21e2ceabaa4d855faf3e7d8c1eb3bdf4
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-03-25 15:03:10 +0200

    321

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const students = ["Mykola", "Lev", "Vlad D.", "Nazar", "Rostislav", "Vlad"]
+
+console.log(students);
+students.push("Yura")//Метод додає в кінець массиву//
+console.log(students);
+students.unshift("Zoryana", "123")//Метод додає на початок массиву//
+console.log(students);
+students.shift();
+students.shift();
+console.log(students);
+students.pop()//Метод видаляє останній елемент з массиву//
+console.log(students);
+
+
+
+
+
